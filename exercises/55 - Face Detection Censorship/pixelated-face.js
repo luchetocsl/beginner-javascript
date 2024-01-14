ecb051a7005d92a4d1b137c5b236a998b370e94f
Luis Martinez <67282267+luchetocsl@users.noreply.github.com>
2024-01-14 18:17:41 -0500

    Summary

--- exercises/55 - Face Detection Censorship/pixelated-face.js
+++ exercises/55 - Face Detection Censorship/pixelated-face.js
@@ -1,3 +1,32 @@
-// The face detection does not work on all browsers and operating systems.
-// If you are getting a `Face detection service unavailable` error or similar,
-// it's possible that it won't work for you at the moment.
+const video = document.querySelector(".webcam");
+
+const canvas = document.querySelector(".video");
+const ctx = canvas.getContext("2d");
+
+const faceCanvas = document.querySelector(".face");
+const faceCtx = faceCanvas.getContext("2d");
+
+const faceDetector = new window.FaceDetector();
+
+async function populateVideo() {
+	const stream = await navigator.mediaDevices.getUserMedia({
+		video: {
+			width: 1280,
+			height: 720,
+		},
+	});
+	video.srcObject = stream;
+	await video.play();
+
+	canvas.width = video.videoWidth;
+	canvas.height = video.videoHeight;
+	faceCanvas.width = video.videoWidth;
+	faceCanvas.height = video.videoHeight;
+}
+
+async function detect() {
+	const faces = await faceDetector.detect(video);
+	console.log(faces);
+}
+
+populateVideo().then(detect);
